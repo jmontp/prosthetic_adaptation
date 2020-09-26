be4003b6ecaf90ac0fca64fa41fdcef7a63afc67
jose-montes1 <jose.montes1@upr.edu>
2020-09-26 12:45:01 -0400

    Added draft for least squares fourier series estimation

--- thigh-angle-estimation.py
+++ thigh-angle-estimation.py
@@ -9,7 +9,7 @@ Created on Wed Sep  2 21:16:47 2020
 #%% Imports and setup
 
 #My own janky imports
-from generators import *
+from generators import get_trials_in_numpy, get_phi_from_numpy, get_step_length
 
 
 
@@ -17,35 +17,47 @@ from generators import *
 import matplotlib.pyplot as plt
 import numpy as np
 
-#Gaussian process regression
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF
+#To get the data
+import h5py
 
-#Fourier Regression
-from symfit import parameters, variables, sin, cos, Fit
 
-#Ref:https://stackoverflow.com/questions/52524919/fourier-series-fit-in-python
-def fourier_series (x,y,f,n=0):
-    """
-    Returns a symbolic fourier series of order `n`.
+#Set a reference to where the dataset is located
+dataset_location = 'local-storage/'
+#Reference to the raw data filename
+filename = 'InclineExperiment.mat'
 
-    :param n: Order of the fourier series.
-    :param x: Independent variable
-    :param f: Frequency of the fourier series
-    """
-    #Make the paremeter objects for all the terms
-    a0, *cos_a=parameters(','.join(['a{}'.format(i) for i in range(0,n+1)]))
-    sin_b = parameters(','.join(['b{}'.format(i) for i in range(1, n + 1)]))
-    c0, *cos_c=parameters(','.join(['c{}'.format(i) for i in range(0,n+1)]))
-    sin_d = parameters(','.join(['d{}'.format(i) for i in range(1, n + 1)]))
-    # Construct the series???
-    series = a0 + sum(ai*cos(i*f*x)+bi*sin(i*f*x) for i, (ai, bi) in enumerate(zip(cos_a, sin_b), start=1))
-    series += c0*y + sum(y*ci*cos(i*f*x)+y*di*sin(i*f*x) for i, (ci, di) in enumerate(zip(cos_c, sin_d), start=1))
-    return series
+raw_walking_data = h5py.File(dataset_location+filename, 'r')
 
-#Performing the PCA
-from sklearn.decomposition import PCA
 
+
+
+#https://stackoverflow.com/questions/4258106/how-to-calculate-a-fourier-series-in-numpy
+#This implementation looks better ngl
+
+
+#My implementation based on Grays least squares estimation
+def get_fourier_coeff (x,y,z,n=0):
+ #flatten all the data out
+ phi=x.reshape(-1)
+ #phi=2*np.pi*x.reshape(-1)
+ step_length=y.reshape(-1)
+ left_hip_angle=z.reshape(-1)
+ 
+ R = [np.ones((len(phi),))]
+ for i in range(n)[1:]:
+     R.append(np.sin(i*phi))
+     R.append(np.cos(i*phi))
+     R.append(step_length*np.cos(i*phi))
+     R.append(step_length*np.sin(i*phi))
+ R = np.array(R).T
+     
+ return np.linalg.solve(R.T @ R, R.T @ left_hip_angle)
+ 
+    
+#Plot the fourier series
+def get_fourier_sum(a,x, Nh):
+   f = np.array([2*a[i]*np.exp(1j*2*i*np.pi*x) for i in range(1,Nh+1)])
+   return f.sum()
 
 
 #Paint the lines with corresponding colors
@@ -61,13 +73,13 @@ datapoints=150
 num_params=8
 
 
-
+#The list of all the fourier coefficients per patient
+parameter_list=[]
 
 #Iterate through all the trials for a test subject
 for subject in raw_walking_data['Gaitcycle'].keys():
     
     print("Doing subject: " + subject)
-    #%% This section is dedicated to setting up the data
     left_hip_total=get_trials_in_numpy(subject)
     phi_total=get_phi_from_numpy(left_hip_total)
     step_length_total=get_step_length(subject)   
@@ -75,31 +87,22 @@ for subject in raw_walking_data['Gaitcycle'].keys():
     print("Got data for: " + subject)
     # This section is dedicated to regression based on fourier series
     #This is mostly from: https://stackoverflow.com/questions/52524919/fourier-series-fit-in-python
-    
-    x, y, z = variables('x, y, z')
-    w, = parameters('w')
-    model_dict = {z: fourier_series(x, y, f=w, n=num_params)}
-    
-    fit = Fit(model_dict, x=phi_total, y=step_length_total, z=left_hip_total)
-    fit_result = fit.execute()
-        
-    print(fit_result)
+    fourier_coeff = get_fourier_coeff(phi_total,step_length_total,left_hip_total,num_params)
+    y_pred = get_fourier_sum(fourier_coeff, phi_total[0], num_params)
     
     #Plot the result
     plt.plot(phi_total[0], left_hip_total[0], color=colors[color_index])
-    plt.plot(phi_total[0], fit.model(x=phi_total[0], y=step_length_total[0],
-                            **fit_result.params).z, color=colors[color_index], ls=':')
+    plt.plot(phi_total[0], y_pred, color=colors[color_index], ls=':')
     color_index=(color_index+1)%len(colors)
     plt.show
     
-    parameters_per_walk_config += [fit_result.params.copy()]
-    parameter_list += fit_result.params.copy().values()
+    parameter_list += fourier_coeff
     
-    
-    #parameters_per_walk_config[-1]['walking_config'] = walking_configuration
     
 #%% This section is dedicated to PCA on the coefficients for the fourier 
 ### transform
+#Performing the PCA
+from sklearn.decomposition import PCA
 
 
 #We need the amount of subjects to reshape the parameter matrix
@@ -140,42 +143,3 @@ fig.show()
 
 
 
-#%% This section is dedicated for regression based on GP
-
-# #Define the kernel, here we are going to use the RBF kernel to start
-# kernel = RBF(length_scale=0.01, length_scale_bounds=(1e-3, 1e1))
-
-# #Need to reshape de data in order to fit to gaussian process
-# phi_reshape = phi[:, np.newaxis]    #(150,) -> (150,1)
-# left_hip_reshape = np.array(left_hip[0])[:, np.newaxis]
-# predict_phi = np.linspace(0,1,1000)
-
-
-# gp = GaussianProcessRegressor(kernel=kernel, alpha=0.0)
-# fit_model = gp.fit(phi_reshape,left_hip[0])
-
-# y_mean, y_cov = fit_model.predict(predict_phi[:, np.newaxis], return_cov=True)
-# y_mean.reshape(-1,1)
-# y_cov.reshape(-1,1)
-
-
-# plt.plot(predict_phi, y_mean, 'k', lw=3, zorder=9)
-# plt.fill_between(predict_phi, y_mean - np.sqrt(np.diag(y_cov)),
-#                  y_mean + np.sqrt(np.diag(y_cov)),
-#                  alpha=0.5, color='k')
-
-
-# plt.plot(predict_phi, y_mean - np.sqrt(np.diag(y_cov)),alpha=0.5, color='k')
-# plt.plot(predict_phi, y_mean + np.sqrt(np.diag(y_cov)),alpha=0.5, color='k')
-
-# plt.show()
-
-
-
-
-
-
-
-
-#Change model fitting to grays algorithm
-#Visualizations
