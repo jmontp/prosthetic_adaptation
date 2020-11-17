0abde3feac7a6fac1ddab66a388d3cd4d1648653
Gray Cortright Thomas, PhD <gray.c.thomas@gmail.com>
2020-11-17 15:26:54 -0500

    starting to redo the pca part

--- pca_visualizer.py
+++ pca_visualizer.py
@@ -29,17 +29,25 @@ server = app.server
 #Here comes the logic
 
 #Load the numpy array from memory that contains the fourier coefficients
-np_parameters = np.load('fourier coefficient matrix.npy')
+Ξ = np.load('fourier coefficient matrix.npy')
+# what is this matrix?
+	# the matrix is literally \Xi
+print(Ξ.shape) # (10, 45)
 
-default_fourier_parameters=np_parameters.shape[1]
+print("ξₐᵥ=", np.mean(Ξ, axis=0))
+Ξ0 = Ξ - np.mean(Ξ, axis=0)
+print("Ξ0", Ξ0)
 
-n_pca_axis=min(np_parameters.shape)
+
+default_fourier_parameters=Ξ.shape[1]
+
+n_pca_axis=min(Ξ.shape)
 
 pca=PCA(n_components=n_pca_axis)
 
-pca.fit(np_parameters)
+pca.fit(Ξ)
 
-fourier_paramaters_pca = pca.transform(np_parameters)
+fourier_paramaters_pca = pca.transform(Ξ)
 
 pca_input_list=[Input('pca'+str(i),'value') for i in range(n_pca_axis)]
 
@@ -55,8 +63,8 @@ pca_sliders=[drc.NamedSlider(
 							id='step-length',
 							min=0,
 							max=2,
-							step=0.01,
-							marks={i: str(i/10) for i in range(21)},
+							step=0.1,
+							marks={i/10:"%.1f"%float(i/10) for i in range(21)},
 							value = 1
 						)]
 
@@ -147,7 +155,7 @@ def update_pca_graph(step_length, *pca_argv):
     #Get the axis for the first three pca vectors
     parameter_tuples = zip(pca.components_,pca_argv)
 
-    parameter=np_parameters[0]+sum([pca_axis*pca_slider_value for pca_axis,pca_slider_value in parameter_tuples])
+    parameter=Ξ[0]+sum([pca_axis*pca_slider_value for pca_axis,pca_slider_value in parameter_tuples])
     #Get the predicted y from the model
     y_pred=get_fourier_prediction(parameter,
                                   phi, 
@@ -155,7 +163,7 @@ def update_pca_graph(step_length, *pca_argv):
                                   num_params)
     # print('Phi is ' + str(phi))
     # print('Step length is ' + str(step_length_array))
-    # print('Params is '+str(np_parameters[0]))
+    # print('Params is '+str(Ξ[0]))
     # print('Predicted y is ' + str(y_pred))
     
     return px.line(x=phi, y=y_pred,title='Fourier Model Prediction', labels={'x': 'Phi', 'y': 'Thigh Angle'})
@@ -166,45 +174,3 @@ def update_pca_graph(step_length, *pca_argv):
 #Run the server
 if __name__=='__main__':
 	app.run_server(debug=True)
-
-
-# #This is the callback that updates the graphs
-# @app.callback(Output('graph-fourier-pca', 'figure'),
-# 			 [Input('pca1','value'),
-# 			  Input('pca2', 'value'), 
-# 		      Input('pca3','value'), 
-# 		      Input('step-length', 'value')])
-# def update_pca_graph(pca1_slider, pca2_slider, pca3_slider, step_length):
-
-    
-#     #Recreate the phi and step length inputs
-#     phi=np.linspace(0,1,150)#.reshape(1,150)
-#     step_length_array=np.full((150,),step_length)
-#     num_params=12
-       
-#     #Get the axis for the first three pca vectors
-#     pca1_axis=pca.components_[0]
-#     pca2_axis=pca.components_[1]
-#     pca3_axis=pca.components_[2]
-#     #Get the predicted y from the model
-#     y_pred=get_fourier_prediction(np_parameters[0]+pca1_slider*pca1_axis+pca2_slider*pca2_axis+ pca3_slider*pca3_axis,
-#                                   phi, 
-#                                   step_length_array,
-#                                   num_params)
-#     # print('Phi is ' + str(phi))
-#     # print('Step length is ' + str(step_length_array))
-#     # print('Params is '+str(np_parameters[0]))
-#     # print('Predicted y is ' + str(y_pred))
-    
-#     return px.line(x=phi, y=y_pred)
-
-
-
-#This is the callback for the explained variance
-#@app.callback(Output('explained-variance', 'figure'))
-# def update_variance_graph():
-# 	return px.line(np.cumsum(pca.explained_variance_ratio_))
-
-
-
-
