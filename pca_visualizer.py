6e9752964de88cc63fe18d3feb96c04b29b912f1
jmontp <jmontp@umich.edu>
2020-12-02 02:36:35 -0400

    Updated vizualization, added assertion based on rmse error

--- pca_visualizer.py
+++ pca_visualizer.py
@@ -4,13 +4,15 @@
 #   Did similar layout to: https://github.com/plotly/dash-svm/blob/master/app.py
 
 
+#Plotting
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import utils.dash_reusable_components as drc
 from dash.dependencies import Input,Output,State
-
 import plotly.express as px
+import pandas as pd
+
 
 import numpy as np
 
@@ -84,19 +86,27 @@ Qinv    = sum([O[:,[i]] @ O[:,[i]].T * np.sqrt(eig[i]) for i in range(len(eig))]
 
 Σ = Ξ0prime.T @ Ξ0prime / (Ξ0prime.shape[0]-1)
 
-ψs, U = np.linalg.eigh(Σ)
+ψinverted, Uinverted = np.linalg.eigh(Σ)
+#Eigenvalues are received from smalles to bigger, make it bigger 
+#to smaller
+ψs = np.flip(ψinverted)
 Ψ = np.diagflat(ψs)
+#If we change the eigenvalues we also need to change the eigenvectors
+U = np.flip(Uinverted, axis=1)
 
+#Run tests to make sure that this is working
 assert(np.linalg.norm(Σ - U @ Ψ @ U.T)< 1e-7 * np.linalg.norm(Σ)) # passes
 for i in range(len(ψs)-1):
-	assert(ψs[i] < ψs[i+1])
+	assert(ψs[i] > ψs[i+1])
 
 #Define the amount principles axis that we want
 η = 6
 ss = []
 
-for i in range (1,η+1):
-	ss.insert(0, Qinv @ (U[:,-i]*np.sqrt(ψs[-i])))
+
+#Convert from the new basis back to the original basis vectors
+for i in range (0,η):
+	ss.append(Qinv @ (U[:,i]*np.sqrt(ψs[i])))
 	# print("At i = {}".format(i))
 	# print("Shape of the Si is")
 	# print(ss[i-1].shape)
@@ -108,7 +118,7 @@ for i in range (1,η+1):
 # print("ξ average")
 # print(ξ_avg.shape)
 # print("Test sum")
-test = ss[0] + ss[1] + ss[2] + ss[3]
+#test = ss[0] + ss[1] + ss[2] + ss[3]
 # print(test)
 # print(test.shape)
 
@@ -117,19 +127,41 @@ test = ss[0] + ss[1] + ss[2] + ss[3]
 # print("Shape of Qinv")
 # print(Qinv.shape)
 
+#Make sure that our new basis meets the criteria that we had
+#To be more specific, make sure that the rms error is close to one for
+#different unit vectors
+num_params=ξ_avg.shape[0]
+#Recreate the phi and step length inputs
+phi=np.linspace(0,1,150)#.reshape(1,150)
+step_length_array=np.full((150,),1)
+for i in range(num_params):
+    ξtest = np.zeros(num_params)
+    ξtest[i] = 1
+    test_deviation_function = get_fourier_prediction(Qinv @ ξtest,
+    											phi,
+    											step_length_array,
+    											12)
+    test_rmse = np.sqrt(np.mean(np.square(test_deviation_function)))
+    print(ξtest)
+    print(test_rmse)
+    #assert(1-1e-3 < abs(test_rmse) < 1+1e-3)
+
+
+
 pca_input_list=[Input('pca'+str(i),'value') for i in range(η)]
 
 
 def update_variance_graph():
- 	return px.line(y=np.cumsum([0]+list(np.flip(ψs[-η:])/sum(ψs))), x=range(η+1), title='Cumulative Sum of variance', labels={'x': 'pca axis', 'y': 'percentage of variance covered'})
+ 	return px.line(y=np.cumsum([0]+list((ψs[0:η])/sum(ψs))), x=range(η+1), title='Cumulative Sum of variance', labels={'x': 'pca axis', 'y': 'percentage of variance covered'})
 
 
 pca_sliders=[]
 
-marker_step = 20
-marker_min = 0
-marker_range = 2
-slider_marks1 = {int(i/marker_step):str(int(i/marker_step)) for i in range(-marker_range*marker_step, (marker_range+1)*marker_step,marker_step)}
+marker_step = 6
+marker_min = 0.8
+marker_range = 1.2
+
+slider_marks1 = {i: "{0:.2f}".format(i) for i in np.append([0],np.linspace(marker_min, marker_range, num=marker_step))}
 print(slider_marks1)
 
 
@@ -144,9 +176,9 @@ pca_sliders=[drc.NamedSlider(
 						)]
 
 
-marker_step = 20
+marker_step = 6
 marker_range = 3
-slider_marks2 = {int(i/marker_step):str(int(i/marker_step)) for i in range(-marker_range*marker_step, (marker_range+1)*marker_step,marker_step)}
+slider_marks2 = {i: str(i) for i in range(-marker_range, marker_range)}
 print(slider_marks2)
 
 
@@ -234,25 +266,77 @@ def update_pca_graph(step_length, *pca_argv):
        
     #Get the axis for the first three pca vectors
     parameter_tuples = zip(ss,pca_argv)
-    print("PCA argv")
-    print(pca_argv)
-    print("Axis")
-    print([i.shape for i in ss])
+    # print("PCA argv")
+    # print(pca_argv)
+    # print("Axis")
+    # print([i.shape for i in ss])
 
+    #Calculate the deviation from the average basis vector
+    deviation_from_average = sum([pca_axis*pca_slider_value for pca_axis,pca_slider_value in parameter_tuples])
 
-    parameter=ξ_avg + sum([pca_axis*pca_slider_value for pca_axis,pca_slider_value in parameter_tuples])
+    average_function = get_fourier_prediction(ξ_avg,
+    										  phi,
+    										  step_length_array,
+    										  num_params)
+
+    deviation_function = get_fourier_prediction(deviation_from_average,
+    											phi,
+    											step_length_array,
+    											num_params)
+
+    rmse = np.sqrt(np.mean(np.square(deviation_function)))
+
+    #print("The rmse for this function from the average is: " + str(rmse))
+
+    parameter=ξ_avg + deviation_from_average
 
     #Get the predicted y from the model
-    y_pred=get_fourier_prediction(parameter,
-                                  phi, 
-                                  step_length_array,
-                                  num_params)
+    # y_pred=get_fourier_prediction(parameter,
+    #                               phi, 
+    #                               step_length_array,
+    #                               num_params)
+    #print("Average function + deviation function - y_pred" + str(np.cumsum(average_function + deviation_function - y_pred)))
+
+    y_pred = average_function + deviation_function
+
     # print('Phi is ' + str(phi))
     # print('Step length is ' + str(step_length_array))
     # print('Params is '+str(Ξ[0]))
     # print('Predicted y is ' + str(y_pred))
     
-    return px.line(x=phi, y=y_pred,title='Fourier Model Prediction', labels={'x': 'Phi', 'y': 'Thigh Angle'})
+
+
+    #Construct a dataframe to ease plotting
+    df = pd.DataFrame({"x": phi,
+    	               "Individualized Function": y_pred, 
+    	               "Average Function": average_function,
+    	               "Deviation Function": deviation_function})
+
+    df_melt = df.melt(id_vars="x", value_vars=["Individualized Function", "Average Function", "Deviation Function"])
+
+    fig = px.line(df_melt, 
+    			  x="x",
+    			  y="value", 
+    			  color='variable',
+    			  title='Fourier Model Prediction', 
+    			  labels={'x': 'Phi', 'value': 'Thigh Angle'})
+
+    fig.update_yaxes(
+    range=[-60,80],  # sets the range of xaxis
+    constrain="domain",  # meanwhile compresses the xaxis by decreasing its "domain"
+	)
+
+    fig.update_layout(legend=dict(
+    orientation="h",
+    yanchor="bottom",
+    y=1.02,
+    xanchor="right",
+    x=1
+	))
+    
+    fig.update_layout(legend_title_text="Deviation RMSE: {0:.2f}    ".format(rmse))
+
+    return fig
 
 
 
