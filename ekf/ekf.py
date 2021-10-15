cefbb5fd31332b9bfa644fa438eeb9b024f37c20
jmontp <jmontp@umich.edu>
2021-10-15 12:11:03 -0400

    Added default ekf implementation and ekf output to the ekf class

--- ekf/ekf.py
+++ ekf/ekf.py
@@ -13,7 +13,7 @@ math_utils.test_pd = True
 
 class Extended_Kalman_Filter:
 
-    def __init__(self,initial_state,initial_covariance,dynamic_model,process_noise,measurement_model,observation_noise):
+    def __init__(self,initial_state,initial_covariance,dynamic_model,process_noise,measurement_model,observation_noise,output_model=None):
         self.dynamic_model = dynamic_model
         self.measurement_model = measurement_model
         self.x = initial_state
@@ -22,18 +22,36 @@ class Extended_Kalman_Filter:
         self.R = observation_noise
         self.calculated_measurement_ = None
         self.num_states = initial_state.shape[0]
+        
+        self.output_model = output_model
+        
+        if self.output_model is not None: 
+            self.output = self.output_model.evaluate_h_func(initial_state)
+        else:
+            self.output = None
+
+
+    #Getter for output
+    def get_output(self):
+        return self.output
 
 
     #Calculate the next estimate of the kalman filter
-    def calculate_next_estimates(self, time_step, control_input_u, sensor_measurements):
+    def calculate_next_estimates(self, time_step, sensor_measurements, control_input_u=0):
         
+        #Run the prediction step
         predicted_state, predicted_covariance = self.preditction_step(time_step)
         self.predicted_state = predicted_state
         self.predicted_covariance = predicted_covariance
         
+        #Run the measurement step
         updated_state, updated_covariance = self.update_step(predicted_state, predicted_covariance, sensor_measurements)
         self.x = updated_state
         self.P = updated_covariance
+
+        # Calculate the output
+        if (self.output_model is not None):
+            self.output = self.output_model.evaluate_h_func(updated_state)
 
         return updated_state, updated_covariance
 
@@ -91,3 +109,81 @@ class Extended_Kalman_Filter:
         math_utils.assert_pd(updated_covariance, "Updated Covariance")
         
         return updated_state, updated_covariance
+
+
+
+
+
+def default_ekf(R=None,Q=None):
+    from .context import kmodel
+    from kmodel.kronecker_model import model_loader
+
+
+    #Phase, Phase Dot, Ramp, Step Length, 5 gait fingerprints
+    state_names = ['phase', 'phase_dot', 'stride_length', 'ramp',
+                    'gf1', 'gf2','gf3', 'gf4', 'gf5']
+
+    ## Load the measurement and output models
+
+    #Initialize the measurement model
+    #Define the joints that you want to import 
+    joint_names = ['jointangles_hip_dot_x','jointangles_hip_x',
+                    'jointangles_knee_dot_x','jointangles_knee_x',
+                    'jointangles_thigh_dot_x','jointangles_thigh_x']
+
+    model_dir = '../../data/kronecker_models/model_{}.pickle'
+
+    models = [model_loader(model_dir.format(joint)) for joint in joint_names]
+    
+    measurement_model = MeasurementModel(state_names,models)
+
+    #Initialize the output model 
+    #Get the torque models from 
+    torque_names = ['jointmoment_hip_x', 'jointmoment_knee_x']
+    torque_models = [model_loader(model_dir.format(torque)) for torque in torque_names]
+
+    output_model = MeasurementModel(state_names, torque_models)
+
+
+    #Initialize gait fingerprint to all zeros
+    initial_gait_fingerprint = np.array([[0.0,0.0,0.0,0.0,0.0]]).T    
+
+    #Setup initial states to zero 
+    #Phase, Phase, Dot, Stride_length, ramp
+    initial_state_partial= np.array([[0.0,0.0,1.0,0.0]]).T
+    initial_state = np.concatenate((initial_state_partial,initial_gait_fingerprint))
+
+    #Generate the initial covariance as being very low
+    #TODO - double check with gray if this was the strategy that converged or not
+    cov_diag = 1e-5
+    initial_state_diag = [cov_diag,cov_diag,cov_diag,cov_diag,
+                          cov_diag,cov_diag,cov_diag,cov_diag,cov_diag]
+    initial_state_covariance = np.diag(initial_state_diag)
+
+
+    #Measurement covarience, Innovation
+    r_diag = [25,25,25,3000,3000,3000]
+    R_default = np.diag(r_diag)
+
+    #Verify if input is none
+    if(R is None):
+        R = R_default
+
+    #Process noise
+    #Phase, Phase, Dot, Stride_length, ramp, gait fingerprints
+
+    q_diag = [0,3e-7,5e-8,1e-8,
+            1e-8,1e-8,1e-8,1e-8,1e-8]
+    Q_default = np.diag(q_diag)
+
+    if(Q is None):
+        Q = Q_default
+
+    ###################
+
+    d_model = GaitDynamicModel()
+
+    ekf_instance = Extended_Kalman_Filter(initial_state,initial_state_covariance, d_model, Q, measurement_model, R, output_model)
+
+    return ekf_instance
+
--- scripts/useful_scripts/simulate_ekf.py
+++ scripts/useful_scripts/simulate_ekf.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+#%%
 #Standard Imports
 import pandas as pd
 import numpy as np
@@ -14,6 +14,7 @@ from context import utils
 from ekf.measurement_model import MeasurementModel
 from ekf.dynamic_model import GaitDynamicModel
 from ekf.ekf import Extended_Kalman_Filter
+from ekf.ekf import default_ekf
 from kmodel.kronecker_model import model_loader
 from kmodel import kronecker_model
 from utils.math_utils import get_rmse
@@ -23,266 +24,219 @@ from utils.math_utils import get_rmse
 
 def simulate_ekf():
     pass
-    #%%
-    file_location = '../../data/flattened_dataport/dataport_flattened_partial_{}.parquet'
-    subject_name = 'AB07'
-    filename = file_location.format(subject_name)
-    print(f"Looking for {filename}")
+#%%
+
+#Process noise
+#Phase, Phase, Dot, Stride_length, ramp, gait fingerprints
+
+q_diag = [0,3e-5,5e-5,1e-4,
+          1e-6,1e-6,1e-6,1e-6,1e-6]
+Q = np.diag(q_diag)
 
 
+#Create the instance for the ekf
+ekf_instace = default_ekf(Q=Q)
 
-    ## Load the models
-    #Define the joints that you want to import 
-    joint_names = ['jointangles_hip_dot_x','jointangles_hip_x',
-                    'jointangles_knee_dot_x','jointangles_knee_x',
-                    'jointangles_thigh_dot_x','jointangles_thigh_x']
+#Get the diagonal entries of the Q matrix
+q_diag = np.diagonal(ekf_instace.Q)
 
-    model_dir = '../../data/kronecker_models/model_{}.pickle'
+#Get the models to get some info from them to plot 
+models = ekf_instace.measurement_model.models
 
-    models = [model_loader(model_dir.format(joint)) for joint in joint_names]
+### Load the datasets
 
-    #Get the torque models from 
-    torque_names = ['jointmoment_hip_x', 'jointmoment_knee_x']
-    torque_model = [model_loader(model_dir.format(torque)) for torque in torque_names]
+#File relative imports
+file_location = '../../data/flattened_dataport/dataport_flattened_partial_{}.parquet'
+subject_name = 'AB07'
+filename = file_location.format(subject_name)
+print(f"Looking for {filename}")
 
+#Read in the parquet dataframe
+total_data = pd.read_parquet(filename)
 
-    ### Load the datasets
-    #Read in the parquet dataframe
-    total_data = pd.read_parquet(filename)
-
-    #Get the joint data to play back
-    joint_data = total_data[joint_names]
-
-
-    #Phase, Phase Dot, Ramp, Step Length, 5 gait fingerprints
-    state_names = ['phase', 'phase_dot', 'stride_length', 'ramp',
-                    'gf1', 'gf2','gf3', 'gf4', 'gf5']
+#Define the joints that you want to import 
+model_names = ['jointangles_hip_dot_x','jointangles_hip_x',
+                'jointangles_knee_dot_x','jointangles_knee_x',
+                'jointangles_thigh_dot_x','jointangles_thigh_x']
 
 
-    #Delete next line if nothing breaks
-    #measurement_names = ["Foot", "Shank", "Foot Dot", "Shank Dot"]
+#Define the output names
+output_names = ['jointmoment_hip_x', 'jointmoment_knee_x']
 
 
-    #Get the ground truth from the datasets
-    ground_truth_labels = ['phase','phase_dot','stride_length','ramp']
-    ground_truth = total_data[ground_truth_labels]
+#Phase, Phase Dot, Ramp, Step Length, 5 gait fingerprints
+state_names = ['phase', 'phase_dot', 'stride_length', 'ramp',
+                'gf1', 'gf2','gf3', 'gf4', 'gf5']
 
 
-    #Initialize the measurement model
-    measurement_model = MeasurementModel(state_names,models)
+#Get the joint data to play back
+joint_data = total_data[model_names]
 
 
-    ############ Setup - Initial Gait Fingerprint
-    ## Can select either 0 initial gait fingerprint or 
-    ## the least square solution gait fingerprint
-    least_squares_gait_fingerprint = models[0].subjects[subject_name]['cross_model_gait_coefficients_unscaled']
-    zero_gait_fingerprint = np.array([[0.0,0.0,0.0,0.0,0.0]]).T
+#Get the ground truth from the datasets
+ground_truth_labels = ['phase','phase_dot','stride_length','ramp']
+ground_truth = total_data[ground_truth_labels]
 
-    initial_gait_fingerprint = zero_gait_fingerprint
+############ Setup - Data Segments
+### Setup data per section constants
+trials = 5                      #How many trials
+steps_per_trial = 10            #Steps per trial
+points_per_step = 150               
+points_per_trial = points_per_step * steps_per_trial
+experiment_point_gap = 107 * points_per_step
+#Skip x amount of steps from the start of the experiments
+skip_steps = 15
+skip_points = skip_steps * points_per_step
+#Make the first section very long to learn gait fingerprint
+first_section_steps = 5
+first_section_points = first_section_steps * points_per_step# + 75
 
+#Calculate the total number of datapoints
+datapoints = points_per_trial * trials + first_section_points
+#Pre-allocate memory
+multiple_step_data = np.zeros((datapoints,len(model_names)))
+multiple_step_ground_truth = np.zeros((datapoints,len(ground_truth_labels)))
 
-    ############ Setup - Initial States
-    #### Pretty much random but reasonable initial states 
-    #Phase, Phase, Dot, Stride_length, ramp
-    initial_state_partial= np.array([[0.0,1.0,1.2,-10.0]]).T
-    initial_state = np.concatenate((initial_state_partial,initial_gait_fingerprint))
-
-    #Generate the initial covariance as being very low
-    #TODO - double check with gray if this was the strategy that converged or not
-    initial_state_diag = [1e-14,1e-14,1e-14,1e-14,
-                            1e-14,1e-14,1e-14,1e-14,1e-14]
-    initial_state_covariance = np.diag(initial_state_diag)
-
-
-
-    ############ Setup - Noise
-    #Measurement covarience, Innovation
-    #Safe
-    r_diag = [25,25,25,3000,3000,3000]
-    #Do not trust the sensor at all, turn off innovation and just use dynamic model
-    #r_diag = [1e8,1e8,1e8,1e8]
-    #Trust the sensors a lot
-    #r_diag = [65,65,65,65]
-    R = np.diag(r_diag)
-
-    #Process noise
-    #['phase','phase_dot','stride_length','ramp']
-    #Turn off gait fingerprint
-    # q_diag = [0,1e-5,1e-6,3e-4,
-    #          1e-28,1e-28,1e-28,1e-28,1e-28]
-
-    #Best for long test
-    # q_diag = [0,1e-7,5e-8,1e-8,
-    #           1e-13,1e-13,1e-13,1e-13,1e-13]
-    #Test cal
-    q_diag = [0,3e-7,5e-8,1e-8,
-            1e-8,1e-8,1e-8,1e-8,1e-8]
-    Q = np.diag(q_diag)
-
-    ###################
-
-    d_model = GaitDynamicModel()
-
-    ekf = Extended_Kalman_Filter(initial_state,initial_state_covariance, d_model, Q, measurement_model, R)
-
-    #We do not include a control input for the EKF, so just set it to zero
-    control_input_u = 0 
-
-    #TODO - talk to Ting Wei about EKF kidnapping robustness
-
-
-    ############ Setup - Data Segments
-    ### Setup data per section constants
-    trials = 5                      #How many trials
-    steps_per_trial = 10            #Steps per trial
-    points_per_step = 150               
-    points_per_trial = points_per_step * steps_per_trial
-    experiment_point_gap = 107 * points_per_step
-    #Skip x amount of steps from the start of the experiments
-    skip_steps = 15
-    skip_points = skip_steps * points_per_step
-    #Make the first section very long to learn gait fingerprint
-    first_section_steps = 5
-    first_section_points = first_section_steps * points_per_step# + 75
-
-    #Calculate the total number of datapoints
-    datapoints = points_per_trial * trials + first_section_points
-    #Pre-allocate memory
-    multiple_step_data = np.zeros((datapoints,len(models)))
-    multiple_step_ground_truth = np.zeros((datapoints,len(ground_truth_labels)))
-
-    #Create the data array based on the setup above
-    for i in range(-1,trials):
-        
-        f = first_section_points
-        
-        if i == -1:
-            multiple_step_data[:f, :] = \
-                joint_data.iloc[:f, :]
-            
-            multiple_step_ground_truth[:f, :] = \
-                ground_truth.iloc[:f, :]
-        else:
-            multiple_step_data[(i*points_per_trial) + f : \
-                                (i*points_per_trial) + f + points_per_trial , :] = \
-                joint_data.iloc[i*experiment_point_gap + skip_points + f:\
-                            i*experiment_point_gap + skip_points + f + points_per_trial, :]
-                
-            multiple_step_ground_truth[i*points_per_trial + f:(i+1)*points_per_trial + f, :] = \
-                ground_truth.iloc[i*experiment_point_gap + skip_points + f: i*experiment_point_gap + points_per_trial + skip_points + f, :]
-
-
-    #Repeat the data
-    repeat_dataset = 5
-    total_datapoints = datapoints * repeat_dataset
-
-    multiple_step_data = np.tile(multiple_step_data, (repeat_dataset,1))
-    multiple_step_ground_truth = np.tile(multiple_step_ground_truth, (repeat_dataset,1))
-
-
-    #Well really, just do all the data
-    # multiple_step_data = joint_data.values
-    # multiple_step_ground_truth = ground_truth.values
-    # total_datapoints = multiple_step_data.shape[0]
-
-    #Calculate the time step based on the fact that phase_dot = dphase/dt
-    #And that dphase = 150 from the fact that we are using the normalized dataset
-    # dt = dt/dphase * dphase
-    time_step = (np.reciprocal(multiple_step_ground_truth[:,1])*1/150).reshape(-1)
-
-    #Create storage for state history
-    state_history = np.zeros((total_datapoints,len(initial_state)))
-    real_measurements = np.zeros((total_datapoints, len(models)))
-    measurement_history = np.zeros((total_datapoints, len(models)))
-    predicted_states = np.zeros((total_datapoints, len(initial_state)))
-    #delta_state = np.zeros((total_datapoints,len(initial_state)))
-    #delta_measurements = np.zeros((total_datapoints, len(models)))
-
-    try:
-        for i in range(total_datapoints):
-            curr_data = multiple_step_data[i].reshape(-1,1)
-            real_measurements[i,:] = curr_data.T
-            next_state = ekf.calculate_next_estimates(time_step[i], control_input_u, curr_data)[0].T
-            state_history[i,:] = next_state
-            measurement_history[i,:] = ekf.calculated_measurement_.T
-            predicted_states[i,:] = ekf.predicted_state.T
-            #delta_state[i,:] = ekf.delta_state.T
-            #delta_measurements[i,:] = ekf.y_tilde.T
-
-            print(f'{i} out of {total_datapoints}')
-    except KeyboardInterrupt:
-        print(f"Interrupted at step {i}")
-        total_datapoints = i+1
-        curr_data = curr_data[:total_datapoints,:]
-        real_measurements = real_measurements[:total_datapoints,:]
-        state_history = state_history[:total_datapoints,:]
-        measurement_history = measurement_history[:total_datapoints,:]
-        predicted_states = predicted_states[:total_datapoints,:]
-        multiple_step_ground_truth = multiple_step_ground_truth[:total_datapoints,:]
-        pass
-
-    #old plotting    
-    #print(state_history[:,0])
-    #plt.plot(state_history[:,:])
-
-
-    rmse_state = [get_rmse(state_history[:,i],multiple_step_ground_truth[:,i]) for i in range(4)]
-
-    rmse_joint_angle = [get_rmse(real_measurements, measurement_history)]    
-
-    #new plotting
-    time_axis = np.arange(total_datapoints)*1/150
-
-    individual_measurements_labels = ['Measured', 'Expected']
-
-    fig,axs = plt.subplots(9,1,sharex=True)
-
-    colors = ['red', 'green', 'orange', 'blue', ]
-
-    axs[-1].set_xlabel("Time (s)")
-
-
-    #Plot measurements
-    for i in range(len(joint_names)):
-        #Plot foot angle
-        fig.suptitle(f'Q: {q_diag}  RMSE State: {rmse_state} RMSE angles: {rmse_joint_angle}')
-        axs[i].plot(time_axis, real_measurements[:,i])
-        axs[i].plot(time_axis, measurement_history[:,i])
-        axs[i].set_ylabel(f"{' '.join(joint_names[i].split('_')[1:-1])}")
-        axs[i].legend(individual_measurements_labels)
-
-    #Plot the gait fingerprints
-    custom_cycler = (cycler(color=['b', 'g', 'r', 'c', 'm']))
-    axs[6].set_prop_cycle(custom_cycler)    
-
-    ls_gf = models[0].subjects[subject_name]['cross_model_gait_coefficients_unscaled']
-    desired_gf = np.repeat(ls_gf,time_axis.shape[0], axis=1).T
-    axs[6].plot(time_axis, desired_gf, ":", lw=2)
-    axs[6].plot(time_axis, state_history[:,4:], lw=2)
-    axs[6].set_ylabel("Gait Fingerprints")
-    axs[6].legend([f'gf{i}' for i in range(1,6)])
+#Create the data array based on the setup above
+for i in range(-1,trials):
     
+    f = first_section_points
+    
+    if i == -1:
+        multiple_step_data[:f, :] = \
+            joint_data.iloc[:f, :]
+        
+        multiple_step_ground_truth[:f, :] = \
+            ground_truth.iloc[:f, :]
+    else:
+        multiple_step_data[(i*points_per_trial) + f : \
+                            (i*points_per_trial) + f + points_per_trial , :] = \
+            joint_data.iloc[i*experiment_point_gap + skip_points + f:\
+                        i*experiment_point_gap + skip_points + f + points_per_trial, :]
+            
+        multiple_step_ground_truth[i*points_per_trial + f:(i+1)*points_per_trial + f, :] = \
+            ground_truth.iloc[i*experiment_point_gap + skip_points + f: i*experiment_point_gap + points_per_trial + skip_points + f, :]
 
-    #Plot the estimated state and ground truth state
-    custom_cycler = (cycler(color=['b', 'g', 'r']))
-    axs[7].set_prop_cycle(custom_cycler)  
 
-    axs[7].plot(time_axis, state_history[:,:3])
-    axs[7].set_ylabel("Estimated State")
-    axs[7].plot(time_axis, multiple_step_ground_truth[:,:3], 
-                linestyle = '--')
-    axs[7].set_ylabel("Ground Truth")
-    axs[7].legend(state_names[:3] + ground_truth_labels[:3])
+#Repeat the data
+repeat_dataset = 5
+total_datapoints = datapoints * repeat_dataset
+
+multiple_step_data = np.tile(multiple_step_data, (repeat_dataset,1))
+multiple_step_ground_truth = np.tile(multiple_step_ground_truth, (repeat_dataset,1))
 
 
+#Well really, just do all the data
+# multiple_step_data = joint_data.values
+# multiple_step_ground_truth = ground_truth.values
+# total_datapoints = multiple_step_data.shape[0]
 
-    #Plot the ground truth states
-    axs[8].plot(time_axis, state_history[:,3])
-    axs[8].plot(time_axis, multiple_step_ground_truth[:,3])
-    axs[8].set_ylabel("Ramp")
-    axs[8].legend(["Estimated Ramp", "Ground Truth Ramp"])
+#Calculate the time step based on the fact that phase_dot = dphase/dt
+#And that dphase = 150 from the fact that we are using the normalized dataset
+# dt = dt/dphase * dphase
+time_step = (np.reciprocal(multiple_step_ground_truth[:,1])*1/150).reshape(-1)
 
-    plt.show()
+#Create storage for state history
+state_history = np.zeros((total_datapoints,len(state_names)))
+real_measurements = np.zeros((total_datapoints, len(model_names)))
+measurement_history = np.zeros((total_datapoints, len(model_names)))
+output_history = np.zeros((total_datapoints, len(output_names)))
+
+try:
+    for i in range(total_datapoints):
+        curr_data = multiple_step_data[i].reshape(-1,1)
+        real_measurements[i,:] = curr_data.T
+        next_state = ekf_instace.calculate_next_estimates(time_step[i], curr_data)[0].T
+        state_history[i,:] = next_state
+        measurement_history[i,:] = ekf_instace.calculated_measurement_.T
+        output_history[i,:] = ekf_instace.get_output().T
+
+        print(f'{i} out of {total_datapoints}')
+
+
+except KeyboardInterrupt:
+    print(f"Interrupted at step {i}")
+    total_datapoints = i+1
+    curr_data = curr_data[:total_datapoints,:]
+    real_measurements = real_measurements[:total_datapoints,:]
+    state_history = state_history[:total_datapoints,:]
+    measurement_history = measurement_history[:total_datapoints,:]
+    multiple_step_ground_truth = multiple_step_ground_truth[:total_datapoints,:]
+    output_history = output_history[:total_datapoints,:]
+    pass
+
+#old plotting    
+#print(state_history[:,0])
+#plt.plot(state_history[:,:])
+
+
+rmse_state = [get_rmse(state_history[:,i],multiple_step_ground_truth[:,i]) for i in range(4)]
+
+rmse_joint_angle = [get_rmse(real_measurements, measurement_history)]    
+
+#new plotting
+time_axis = np.arange(total_datapoints)*1/150
+
+individual_measurements_labels = ['Measured', 'Expected']
+
+fig,axs = plt.subplots(10,1,sharex=True)
+
+colors = ['red', 'green', 'orange', 'blue', ]
+
+axs[-1].set_xlabel("Time (s)")
+
+
+#Plot measurements
+for i in range(len(model_names)):
+    #Plot foot angle
+    fig.suptitle(f'Q: {q_diag}  RMSE State: {rmse_state} RMSE angles: {rmse_joint_angle}')
+    axs[i].plot(time_axis, real_measurements[:,i])
+    axs[i].plot(time_axis, measurement_history[:,i])
+    axs[i].set_ylabel(f"{' '.join(model_names[i].split('_')[1:-1])}")
+    axs[i].legend(individual_measurements_labels)
+
+#Plot the output
+output_index = 6
+axs[output_index].plot(time_axis, output_history)
+axs[output_index].set_ylabel("Torque Outputs")
+axs[output_index].legend(output_names)
+
+
+#Plot the gait fingerprints
+gait_fingerprint_index = 7
+custom_cycler = (cycler(color=['b', 'g', 'r', 'c', 'm']))
+axs[gait_fingerprint_index].set_prop_cycle(custom_cycler)    
+
+ls_gf = models[0].subjects[subject_name]['cross_model_gait_coefficients_unscaled']
+desired_gf = np.repeat(ls_gf,time_axis.shape[0], axis=1).T
+axs[gait_fingerprint_index].plot(time_axis, desired_gf, ":", lw=2)
+axs[gait_fingerprint_index].plot(time_axis, state_history[:,4:], lw=2)
+axs[gait_fingerprint_index].set_ylabel("Gait Fingerprints")
+axs[gait_fingerprint_index].legend([f'gf{i}' for i in range(1,6)])
+
+
+#Plot the estimated state and ground truth state
+ground_truth_index = 8
+custom_cycler = (cycler(color=['b', 'g', 'r']))
+axs[ground_truth_index].set_prop_cycle(custom_cycler)  
+
+axs[ground_truth_index].plot(time_axis, state_history[:,:3])
+axs[ground_truth_index].set_ylabel("Estimated State")
+axs[ground_truth_index].plot(time_axis, multiple_step_ground_truth[:,:3], 
+            linestyle = '--')
+axs[ground_truth_index].set_ylabel("Ground Truth")
+axs[ground_truth_index].legend(state_names[:3] + ground_truth_labels[:3])
+
+
+
+#Plot the ground truth states
+ramp_index = 9
+axs[ramp_index].plot(time_axis, state_history[:,3])
+axs[ramp_index].plot(time_axis, multiple_step_ground_truth[:,3])
+axs[ramp_index].set_ylabel("Ramp")
+axs[ramp_index].legend(["Estimated Ramp", "Ground Truth Ramp"])
+
+plt.show()
 
 
 
